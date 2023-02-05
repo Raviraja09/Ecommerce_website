207e3a267c6bd28df6bcab99f41521ac8407b669
Raviraja09 <rajavysyaraju09@gmail.com>
2023-02-05 13:20:12 +0530

    showing products in cart

---
+++ src/components/CartPage.js
@@ -0,0 +1,16 @@
+const CartPage = ({ Cartitems }) => {
+  return (
+    <div>
+      <h2>Your Cart</h2>
+      <ul>
+        {items.map((item) => (
+          <li key={item.name}>
+            {item.name} - ${item.price}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+export default CartPage;
+  
